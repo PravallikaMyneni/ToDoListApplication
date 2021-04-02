b49fb6008d6c0adcc831552ad133d7103bda1c1b
PravallikaMyneni <pravallika.myneni@gmail.com>
2021-04-02 10:31:37 +0530

    Adding error validation for add task

--- src/AddToDo.js
+++ src/AddToDo.js
@@ -19,30 +19,48 @@ const AddToDo = (props) => {
         todoDate: "",
         todoStatus: false
     });
-
+    const [errorObj, updateErrorState] = useState({
+        "nameError": false,
+        "dateError": false
+    });
     //onChange handlers for text,date
     const handleTextChange = (event) => {
         setToDoItem({ ...toDoItem, todoText: event.target.value });
+        updateErrorState({...errorObj,nameError:false});
     };
     const handleDateChange = (event) => {
         setToDoItem({ ...toDoItem, todoDate: event.target.value });
+        updateErrorState({...errorObj,dateError:false});
     };
 
     const onClickOfAdd = (event) => {
         var idVal = uuid.v1();
-        toDoItem.todoId = idVal;
-        setToDoItem({ ...toDoItem, todoId: idVal });
-        props.addNewToDo(toDoItem);
-        setToDoItem((prevState)=>{ return{
-            todoId:"",
-            todoText: "",
-            todoDate: "",
-            todoStatus: false
-        }});
-
+        if (toDoItem.todoText !== "" && toDoItem.todoDate !== "") {
+            updateErrorState({
+                "nameError": false,
+                "dateError": false
+            });
+            toDoItem.todoId = idVal;
+            setToDoItem({ ...toDoItem, todoId: idVal });
+            props.addNewToDo(toDoItem);
+            setToDoItem((prevState) => {
+                return {
+                    todoId: "",
+                    todoText: "",
+                    todoDate: "",
+                    todoStatus: false
+                }
+            });
+        } else{
+            if(toDoItem.todoText === "")
+              updateErrorState({...errorObj,nameError:true});
+            if(toDoItem.todoDate === "")
+              updateErrorState({...errorObj,dateError:true});
+        }
+        
     };
  
-
+    
     return (
         <div className="app-child-left">
             <div className="add-detail">
@@ -52,11 +70,13 @@ const AddToDo = (props) => {
                 <label>TODO task</label>
                 <input type="text" name="toDoText" value ={toDoItem.todoText} onChange={handleTextChange}
                        placeholder ="Todo Task"  />
+                {errorObj.nameError === true && <p>Please enter task</p>}
             </div>
             <div className ="display-vertical add-detail">
                 <label>Task Date</label>
                 <input type="date" id="start" name="toDoDate" value = {toDoItem.todoDate} 
                     min={minDateValue} onChange={handleDateChange}></input>
+                {errorObj.dateError === true && <p>Please select date</p>}
             </div>
             <div>
                 <button className = "add-todo-btn" onClick={onClickOfAdd}>ADD</button>
